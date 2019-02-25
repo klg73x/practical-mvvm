161d447e40bdb2eec2c7a3599de019beddd06a0b
Kevin Gagnon <klg73@outlook.com>
2019-02-25 16:06:40 -0500

    Moved the data binding to the fields in XAML for the details

--- JoeCoffeeStore.StockManagement.App/View/CoffeeDetailView.xaml.cs
+++ JoeCoffeeStore.StockManagement.App/View/CoffeeDetailView.xaml.cs
@@ -32,28 +32,29 @@ namespace JoeCoffeeStore.StockManagement.App.View
 
         void CoffeeDetailView_Loaded(object sender, RoutedEventArgs e)
         {
-            LoadData();
+            //LoadData();
+            this.DataContext = SelectedCoffee;
         }
 
-        private void LoadData()
-        {
-            CoffeeNameLabel.Content = SelectedCoffee.CoffeeName;
-            CoffeeIdTextBox.Text = SelectedCoffee.CoffeeId.ToString();
-            CoffeeDescriptionTextBox.Text = SelectedCoffee.Description;
-            CoffeePriceTextBox.Text = SelectedCoffee.Price.ToString();
-            StockAmountTextBox.Text = SelectedCoffee.AmountInStock.ToString();
-            FirstTimeAddedTextBox.Text = SelectedCoffee.FirstAddedToStockDate.ToShortDateString();
-            if (SelectedCoffee is SuperiorCoffee)
-                ExtraDescriptionTextBox.Text = (SelectedCoffee as SuperiorCoffee).ExtraDescription;
-            else
-                ExtraDescriptionTextBox.Text = "NA";
+        //private void LoadData()
+        //{
+        //    CoffeeNameLabel.Content = SelectedCoffee.CoffeeName;
+        //    CoffeeIdTextBox.Text = SelectedCoffee.CoffeeId.ToString();
+        //    CoffeeDescriptionTextBox.Text = SelectedCoffee.Description;
+        //    CoffeePriceTextBox.Text = SelectedCoffee.Price.ToString();
+        //    StockAmountTextBox.Text = SelectedCoffee.AmountInStock.ToString();
+        //    FirstTimeAddedTextBox.Text = SelectedCoffee.FirstAddedToStockDate.ToShortDateString();
+        //    if (SelectedCoffee is SuperiorCoffee)
+        //        ExtraDescriptionTextBox.Text = (SelectedCoffee as SuperiorCoffee).ExtraDescription;
+        //    else
+        //        ExtraDescriptionTextBox.Text = "NA";
 
-            BitmapImage img = new BitmapImage();
-            img.BeginInit();
-            img.UriSource = new Uri("/JoeCoffeeStore.StockManagement.App;component/Images/coffee" + SelectedCoffee.CoffeeId + ".jpg", UriKind.Relative);
-            img.EndInit();
-            CoffeeImage.Source = img;
-        }
+        //    BitmapImage img = new BitmapImage();
+        //    img.BeginInit();
+        //    img.UriSource = new Uri("/JoeCoffeeStore.StockManagement.App;component/Images/coffee" + SelectedCoffee.CoffeeId + ".jpg", UriKind.Relative);
+        //    img.EndInit();
+        //    CoffeeImage.Source = img;
+        //}
 
         private void DeleteCoffeeButton_Click(object sender, RoutedEventArgs e)
         {
